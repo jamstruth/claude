2a2aeea8ab6f0388ff9a3a736985469775fc1f10
James Struthers <jamstruth18@gmail.com>
2021-02-01 20:45:00 +0000

    Rename test_low_level_library to test_scalar_gradient so that a single test class doesn't get too large Adding tests for scalar_gradient_x_matrix and scalar_gradient_x_primitive

--- tests/test_scalar_gradient.py
+++ tests/test_scalar_gradient.py
@@ -2,11 +2,12 @@ import claude_low_level_library as low_level
 import unittest
 import numpy as np
 
-class LowLevelLibraryTest(unittest.TestCase):
+
+class ScalarGradientTests(unittest.TestCase):
 
     def test_scalar_gradient_x_all_zeroes(self):
         # Arrange
-        a = np.zeros((3,3,3))
+        a = np.zeros((3, 3, 3))
         nlon = 3
         i = 0
         j = 0
@@ -21,14 +22,14 @@ class LowLevelLibraryTest(unittest.TestCase):
 
     def test_scalar_gradient_x_middle_value(self):
         # Arrange
-        a = np.zeros((3,3,3))
+        a = np.zeros((3, 3, 3))
         nlon = 4
         i = 1
         j = 1
         k = 1
-        a[1,0,1] = 0
-        a[1,1,1] = 2
-        a[1,2,1] = 4
+        a[1, 0, 1] = 0
+        a[1, 1, 1] = 2
+        a[1, 2, 1] = 4
         dx = np.array([1, 1, 1, 1])
 
         expected_result = 4
@@ -39,14 +40,14 @@ class LowLevelLibraryTest(unittest.TestCase):
 
     def test_scalar_gradient_x_middle_value_larger_distance(self):
         # Arrange
-        a = np.zeros((3,3,3))
+        a = np.zeros((3, 3, 3))
         nlon = 4
         i = 1
         j = 1
         k = 1
-        a[1,0,1] = 0
-        a[1,1,1] = 2
-        a[1,2,1] = 4
+        a[1, 0, 1] = 0
+        a[1, 1, 1] = 2
+        a[1, 2, 1] = 4
         dx = np.array([1, 2, 1, 1])
 
         expected_result = 2
@@ -54,17 +55,17 @@ class LowLevelLibraryTest(unittest.TestCase):
         result = low_level.scalar_gradient_x(a, dx, nlon, i, j, k)
         # Assert
         self.assertEqual(expected_result, result)
-    
+
     def test_scalar_gradient_x_middle_value_smaller_distance(self):
         # Arrange
-        a = np.zeros((3,3,3))
+        a = np.zeros((3, 3, 3))
         nlon = 4
         i = 1
         j = 1
         k = 1
-        a[1,0,1] = 0
-        a[1,1,1] = 2
-        a[1,2,1] = 4
+        a[1, 0, 1] = 0
+        a[1, 1, 1] = 2
+        a[1, 2, 1] = 4
         dx = np.array([1, 0.5, 1, 1])
 
         expected_result = 8
@@ -75,34 +76,65 @@ class LowLevelLibraryTest(unittest.TestCase):
 
     def test_scalar_gradient_x_far_left_wraparound(self):
         # Arrange
-        a = np.zeros((4,4,4))
+        a = np.zeros((4, 4, 4))
         nlon = 4
         i = 0
         j = 0
         k = 0
-        a[0,0,0] = 0
-        a[0,1,0] = 2
-        a[0,3,0] = 4
-        dx = np.array([1, 1, 1, 1])  
-        expected_result = -2
-        # Act
-        result = low_level.scalar_gradient_x(a, dx, nlon, i, j, k)
-        # Assert
-        self.assertEqual(expected_result, result)
-    
-    def test_scalar_gradient_x_far_right_wraparound(self):
-        # Arrange
-        a = np.zeros((4,4,4))
-        nlon = 3
-        i = 3
-        j = 3
-        k = 3
-        a[3,0,3] = 0
-        a[3,2,3] = 2
-        a[3,3,3] = 4
+        a[0, 0, 0] = 0
+        a[0, 1, 0] = 2
+        a[0, 3, 0] = 4
         dx = np.array([1, 1, 1, 1])
         expected_result = -2
         # Act
         result = low_level.scalar_gradient_x(a, dx, nlon, i, j, k)
         # Assert
         self.assertEqual(expected_result, result)
+
+    def test_scalar_gradient_x_far_right_wraparound(self):
+        # Arrange
+        a = np.zeros((4, 4, 4))
+        nlon = 3
+        i = 3
+        j = 3
+        k = 3
+        a[3, 0, 3] = 0
+        a[3, 2, 3] = 2
+        a[3, 3, 3] = 4
+        dx = np.array([1, 1, 1, 1])
+        expected_result = -2
+        # Act
+        result = low_level.scalar_gradient_x(a, dx, nlon, i, j, k)
+        # Assert
+        self.assertEqual(expected_result, result)
+
+    def test_scalar_gradient_x_matrix_basic_3d_array(self):
+        a = np.array([[[3,0], [4,0], [7,0]],
+                      [[7,0], [6,0], [2,0]],
+                      [[8,0], [8,0], [8,0]]])
+        dx = np.array([1, 0.5, 2])
+
+        expected_results = np.array([[[-3,0], [4,0], [-1,0]],
+                                     [[8,0], [-10,0], [2,0]],
+                                     [[0,0], [0,0], [0,0]]])
+
+        results = low_level.scalar_gradient_x_matrix(a, dx)
+
+        self.assertTrue(np.array_equal(expected_results, results), msg=f"expected: {expected_results}, actual: {results}")
+
+    def test_scalar_gradient_x_matrix_primitive(self):
+        a = np.array([[[3.0,0], [4,0], [7,0]],
+                      [[7,0], [6,0], [2,0]],
+                      [[3,0], [4,0], [7,0]],
+                      [[8,0], [8,0], [8,0]]])
+        dx = np.array([1, 0.5, 2, 1])
+
+        expected_results = np.array([[[0,0], [0,0], [0,0]],
+                                     [[8,0], [-10,0], [2,0]],
+                                     [[-1.5,0], [2,0], [-0.5,0]],
+                                     [[0,0], [0,0], [0,0]]])
+
+        results = low_level.scalar_gradient_x_matrix_primitive(a, dx)
+
+        self.assertTrue(np.array_equal(expected_results, results), msg=f"expected: {expected_results}, actual: {results}")
+
