Correct tests for primitive scalar gradient y

tests/test_scalar_gradient.py
@@ -181,7 +181,7 @@ class ScalarGradientTests(unittest.TestCase):
                                      [[0, 0], [0, 0], [0, 0]],
                                      [[1, 0], [2, 0], [6, 0]],
                                      [[10, 0], [8, 0], [2, 0]]])
-        results = low_level.scalar_gradient_y_matrix(a, dy)
+        results = low_level.scalar_gradient_y_matrix_primitive(a, dy)
         self.assertTrue(np.array_equal(expected_results, results),
                         msg=f"expected: {expected_results}, actual: {results}")
 
@@ -196,6 +196,6 @@ class ScalarGradientTests(unittest.TestCase):
                                      [[0, 0], [0, 0], [0, 0]],
                                      [[0.5, 0], [1, 0], [3, 0]],
                                      [[5, 0], [4, 0], [1, 0]]])
-        results = low_level.scalar_gradient_y_matrix(a, dy)
+        results = low_level.scalar_gradient_y_matrix_primitive(a, dy)
         self.assertTrue(np.array_equal(expected_results, results),
                         msg=f"expected: {expected_results}, actual: {results}")